013f1ee99e134c40bb29a5956f57e668b44649e6
Grillkol <96018357+Grillkol@users.noreply.github.com>
2024-04-03 01:21:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const videoUrls = [
 ];
 
 function generateGallery() {
-    const galleryContainer = document.getElementById("boxesvideo");
+    const galleryContainer = document.getElementById("boxesvideos");
 
     // Iterate over the video URLs and create a box for each
     videoUrls.forEach(url => {
